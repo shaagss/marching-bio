50d8218374f529bfd25526b9dfbd9944ddb87b9a
Jacob Cogar <shags6669@gmail.com>
2026-07-08 16:12:52 -0500

    Setup for login functionality

--- public/javascript.js
+++ public/javascript.js
@@ -1,7 +1,18 @@
-async function main() {
+console.log("JavaScript file is successfully connected!");
+
+async function special() {
     const {requestLogin} = await import('../api/request-login.mjs');
 
     requestLogin('shags6669@gmail.com');
 }
 
-main();
+const dialog = document.getElementById('login');
+
+function openLogin() {
+    dialog.showModal();
+}
+
+function closeLogin() {
+    dialog.close();
+}
+
